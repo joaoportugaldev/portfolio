62871fa09efceca59c35fc1911ee757366832480
João Portugal <joaosportugal@hotmail.com>
2024-11-26 21:11:21 -0300

    added: some skills, descriptions and logos

--- src/components/Sections/Skills/index.tsx
+++ src/components/Sections/Skills/index.tsx
@@ -3,24 +3,80 @@ import SectionTitle from "../../SectionTitle";
 import SimpleText from "../../SimpleText";
 import Flex from "../../../layout/Flex";
 import { useTranslation } from "react-i18next";
+import * as S from "./styles";
+import next_js from "../../../assets/next_js_logo.svg";
+import react_js from "../../../assets/react_js_logo.svg";
+import ts_logo from "../../../assets/ts_logo.svg";
+import node_js_logo from "../../../assets/node_js_logo.svg";
 
 export default function Skills() {
   const { t } = useTranslation();
+  const skills = [
+    {
+      title: "Next JS",
+      description: t("next_js_description"),
+      image: next_js,
+    },
+    {
+      title: "React JS",
+      description: t("react_js_description"),
+      image: react_js,
+    },
+    {
+      title: "Typescript",
+      description: t("typescript_description"),
+      image: ts_logo,
+    },
+    {
+      title: "Node JS",
+      description: t("node_js_description"),
+      image: node_js_logo,
+    },
+    {
+      title: "Tailwind CSS",
+      description: t("tailwind_css_description"),
+      image: next_js,
+    },
+    {
+      title: "Styled Components",
+      description: t("styled_components_description"),
+      image: next_js,
+    },
+    {
+      title: "Sass",
+      description: t("sass_description"),
+      image: next_js,
+    },
+    {
+      title: "PostgresSQL",
+      description: t("postgresql_description"),
+      image: next_js,
+    },
+    {
+      title: "Ubidots",
+      description: t("ubidots_description"),
+      image: next_js,
+    },
+  ];
 
   return (
-    <Section padding="2rem 0 2rem 0" id="skills">
+    <Section padding="2rem 0 2rem 0" id="skills" bgColor="softDark" >
       <SectionTitle text={t("skills")} />
-      <Flex gap="2rem" align="center" textAlign="start">
-        <Flex>
-          <h2>Next JS</h2>
-          <SimpleText fontSize="largeText" maxWidth="900px">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            voluptate alias ipsum saepe sed minima fugiat, mollitia aliquam
-            magnam ratione, odio deleniti? Excepturi praesentium asperiores
-            ipsam maxime debitis sunt neque?
-          </SimpleText>
-        </Flex>
-        <div style={{backgroundColor: "gray", width: "400px", height: "150px"}}></div>
+
+      <Flex direction="column" gap="2rem" width="100%">
+        {skills.map((skill) => (
+          <Flex gap="2rem" align="start" textAlign="start" width="100%" >
+            <Flex direction="column" align="start" gap="1rem" width="100%">
+              <S.Title>{skill.title}</S.Title>
+              <SimpleText fontSize="largeText" maxWidth="900px">
+                {skill.description}
+              </SimpleText>
+            </Flex>
+            <S.ImageContainer>
+              <img src={skill.image} alt={skill.title} />
+            </S.ImageContainer>
+          </Flex>
+        ))}
       </Flex>
     </Section>
   );
---
+++ src/components/Sections/Skills/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: 2rem;
+  color: ${(props) => props.theme.colors.primary};
+`;
+
+export const ImageContainer = styled.div`
+    width: 400px;
+    height: 150px;
+`
