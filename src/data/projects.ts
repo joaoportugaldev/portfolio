9c7528a727e6977bff4df51b9be239a24c894e12
João Portugal <joaosportugal@hotmail.com>
2024-12-30 19:24:51 -0300

    added: two more projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,8 @@
 import portfolio_home from "../assets/portfolio_home.jpg";
 import efood_home from "../assets/efood_home.jpg";
 import todo_app_home from "../assets/todo_app_home.jpg";
+import disney_movie_page_home from "../assets/disney_movie_page.jpg";
+import contact_list_home from "../assets/contact_list_home.jpg";
 
 const projects = [
   {
@@ -29,6 +31,24 @@ const projects = [
     repoLink: "https://github.com/joaoportugaldev/todo-app.git",
     productionLink: null,
     image: todo_app_home,
+  },
+  {
+    title: "project_disney_movie_page__title",
+    description: "project_disney_movie_page__description",
+    finished: true,
+    finishDate: "2023-06-06T10:59:00-03:00",
+    repoLink: "https://github.com/joaoportugaldev/disney-movie-page.git",
+    productionLink: "https://project-disney-movie-page.vercel.app/",
+    image: disney_movie_page_home,
+  },
+  {
+    title: "project_contact_list__title",
+    description: "project_contact_list__description",
+    finished: true,
+    finishDate: "2023-10-05T12:55:00-03:00",
+    repoLink: "https://github.com/joaoportugaldev/lista-de-contatos.git",
+    productionLink: "https://project-contact-list.vercel.app/",
+    image: contact_list_home,
   },
 ];
 
